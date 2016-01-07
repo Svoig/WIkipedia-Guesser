4fb758ab1ede774b859d93ab5b5084bf0070939e
svoig <harrisonccole@gmail.com>
2016-01-06 22:40:25 -0800

    Added more excluded URLs to image getter and cleaned up a bit

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
-var endPoint = "https://en.wikipedia.org/w/api.php";
-
-var rand = "/w/api.php?action=query&list=random&format=json&rnnamespace=0";
-
-
-var lastTitle = '';
 
 //Eventually accept argument for number of decoy titles to get
 function ArticleGetter() {
-
+	//API endpoint
 	this.endPoint = "https://en.wikipedia.org/w/api.php";
-
+	//URL for random page
 	this.rand = "/w/api.php?action=query&list=random&format=json&rnnamespace=0";
-
+	//Store last title generated, to get the article's image
 	this.lastTitle =  '';
 
 	this.toUrl = function(str) {
@@ -20,13 +14,13 @@ function ArticleGetter() {
 	};
 
 	this.render = function() {
+
 		var promise = $.ajax({
-		url: endPoint + rand,
+		url: this.endPoint + this.rand,
 		dataType: 'jsonp',
 		headers: {'Api-User-Agent': 'WikiGuesser/0.1; harrisonccole@gmail.com', "Content Type": "application/json; charset=UTF-8"}
 		});
-		//BUG: Sometimes get 'Cannot read property "bind" of undefined @main.js:30'
-		//I think it's when a page has no picture. I call imgTitlePromise.render, which tries to find a randomPage and randomImage method within its own scope. I'll have to think of a better way to handle this. Global...?
+
 		var boundRandomPage = this.randomPage.bind(this);
 		var boundRandomImage = this.randomImage.bind(this);
 
@@ -38,19 +32,23 @@ function ArticleGetter() {
 	};
 
 	this.randomPage = function(results) {
+
 		key = results.query.random[0];
+
 		var randTitle = "<h3>"+key.title+"</h3>";
+
 		$("#random").append(randTitle);
 
 		this.lastTitle = this.toUrl(key.title);
 	};
 
 	this.randomImage = function(results) {
+
 		var urlEnd = "?action=query&format=json&titles=" + this.lastTitle + "&prop=images&imlimt=1"; 
 		
 
 		var imgTitlePromise =  $.ajax({
-			url: endPoint + urlEnd,
+			url: this.endPoint + urlEnd,
 			type: "GET",
 			contentType: "application/javascript",
 			dataType: "jsonp",
@@ -79,10 +77,10 @@ function ArticleGetter() {
 				if($("#random").innerHTML !== '') {
 					$("#random").html('');
 				}
-				return "No picture - skipped";
+				return;
 			}
-
-			var imgFileUrl = endPoint + "?action=query&format=json&prop=imageinfo&iiurlwidth=540&iiurlheight=360&iiprop=url&titles=" + imgTitlePromise.imgTitle;
+			//Can't use this.endPoint because of binding
+			var imgFileUrl = randArticle.endPoint + "?action=query&format=json&prop=imageinfo&iiurlwidth=540&iiurlheight=360&iiprop=url&titles=" + imgTitlePromise.imgTitle;
 			console.log("Trying to get image: ", imgFileUrl);
 
 			var imgPromise = $.ajax({
@@ -98,28 +96,38 @@ function ArticleGetter() {
 
 				if (imgTitlePromise.skip === true) {
 				console.log("No picture - skipped");
-				return "No picture - skipped";
+				return;
 				}
-
-				var filter = {'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Ambox_important.svg/360px-Ambox_important.svg.png': '!',
+				//Filter out icons and other unwanted images
+				//Not the best way of doing things, but it works
+				var filter = {
+					'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Ambox_important.svg/360px-Ambox_important.svg.png': '!',
 				 'https://upload.wikimedia.org/wikipedia/commons/7/74/Red_Pencil_Icon.png':'pencil',
 				  'https://upload.wikimedia.org/wikipedia/en/thumb/6/62/PD-icon.svg/360px-PD-icon.svg.png': 'noCopy',
 				   'https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Disambig_gray.svg/472px-Disambig_gray.svg.png': 'usb',
-				    'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Commons-logo.svg/268px-Commons-logo.svg.png': 'wikimedia'};
+				    'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Commons-logo.svg/268px-Commons-logo.svg.png': 'wikimedia',
+					'https://upload.wikimedia.org/wikipedia/en/thumb/4/48/Folder_Hexagonal_Icon.svg/418px-Folder_Hexagonal_Icon.svg.png': 'folder',
+					'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Nuvola_apps_kruler.svg/360px-Nuvola_apps_kruler.svg.png': 'ruler',
+					'https://upload.wikimedia.org/wikipedia/commons/2/28/Crystal_kchart.png': 'crystal_kchart',
+					'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Mplayer.svg/360px-Mplayer.svg.png': 'film',
+					'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/DAB_list_gray.svg/472px-DAB_list_gray.svg.png': 'three_arrows'
+				};
 
 				//Saves a lot of typing and confusion
 				imgLocation = results.query.pages[Object.keys(results.query.pages)[0]].imageinfo[0];
 
-				//Prevent the image from rendering if it's one of the excluded urls, ie the ones in the filter array
+				//Prevent the image from rendering if it's one of the excluded URLs, i.e. the ones in the filter array
 				if (imgLocation.thumburl in filter) {
 					console.log("Excluded url, reloading");
+					//Get rid of false positive title
 					$("#random").html('');
 					randArticle.render();
-					return "ERROR, no picture on page. Reloading";
+					return;
 					
 				};
 
 				var imgUrl = imgLocation.thumburl;
+				//Log the URL in case it should be excluded
 				console.log(imgUrl);
 
 				var newImg = $("<img src='" + imgUrl + "'>");
