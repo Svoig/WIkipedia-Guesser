4806afe5b9ec9e9632004606d98efe0d10669605
svoig <harrisonccole@gmail.com>
2016-03-03 05:46:00 -0800

    Got almost everything working, but I haven't been able to figure out how to get the image to render after several passes... Latest try was with a generator, which broke everyting a little.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,41 +1,96 @@
-var express = require('express');
-var router = express.Router();
-var app = require('../app.js');
-var ArticleGetter = require('../src/articlegetter.js');
+"use strict";
 
+const express = require('express');
+const router = express.Router();
+const app = require('../app.js');
+const ArticleGetter = require('../src/articlegetter.js');
 
+//Get a random page and its image until you find one that works or you hit the repeat limit
+function randLoop(limit) {
+	//Stop looping as soon as you get a 0 for the limit
+	if(limit === 0) return null;
+
+	console.log("randLoop's limit is ", limit);
+
+	const AG = new ArticleGetter();
+
+	const promise = new Promise(function(resolve, reject) {
+
+		AG.randomPage()
+		.then(AG.randomImage)
+		.then(function(data) {
+			console.log("In randLoop, AG.imgUrl is: ,", AG.imgUrl);
+		
+			resolve(data);
+
+		})
+		.catch(function(err) {
+			console.log("Caught an error in randLoop!! ", err);
+			console.log("Typeof err is: ", typeof err);
+			if (typeof err !== "object") {
+				console.log("Got a string error. Reducing limit by 1");
+				//randLoop(limit-1);
+			} else {
+				console.log("Err is in randLoop(): ", err);
+
+				reject(err);
+			}
+		});
+
+	});
+
+	return promise;
+}
+
+function* randGen(limit) {
+	console.log("in randGen");
+	let inThen = false;
+	console.log("randGet's limit is ", limit);
+
+	for (var i = 0; i < limit; i++) {
+		console.log("In randGen at #", i);
+		if (inThen) return "Done!";
+		const rand = yield randLoop()
+		.then(function(data) {
+			res.render('index.hbs', {
+			title: "WikiGuesser", 
+			randTitle: data.imgTitle,
+			pitcher: data.imgUrl
+			});
+		})
+		.catch(function(err) {
+			console.log("Error from randLoop: ", err);
+
+			res.render("error.hbs", {
+				error: err
+			});
+		});
+	}
+}
 
 /* GET home page. */
 router.get('/random', function(req, res, next) {
 	console.log("Getting /random");
 	const articleGetter = new ArticleGetter();
 
-	articleGetter.randomPage()
-	.then(articleGetter.randomImage)
+	randGen();
+	randGen();
+	/*randLoop(5)
 	.then(function(data) {
-
-		console.log("In /random, AG.imgUrl is... ", articleGetter.imgUrl);
-
+		console.log("randLoop gave the data: ", data);
 		res.render('index.hbs', {
 			title: "WikiGuesser",
-			randTitle: articleGetter.randTitle,
-			pitcher: articleGetter.imgUrl
+			randTitle: data.imgTitle,
+			pitcher: data.imgUrl
 		});
 	})
 	.catch(function(err) {
+		console.log("Error from randLoop: ", err);
 
-		if (err === true && articleGetter.skip === true) {
-			articleGetter.render();
-		} else {
-			console.log("err is ", err, " and articleGetter.skip is ", articleGetter.skip);
-			res.render('error.hbs', {
+		res.render("error.hbs", {
 			error: err
-			});
-		}
-
-
-	});
-
+		});
+	});*/
 	
 
 });
