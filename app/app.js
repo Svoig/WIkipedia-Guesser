5677ff79b7c3d886715f03b6e4f5a95dcabccbb1
svoig <harrisonccole@gmail.com>
2016-05-26 20:56:45 -0700

    Just changed port number. Trying to revive Heroku by redeploying

--- app/app.js
+++ app/app.js
@@ -69,7 +69,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-const port = (process.env.PORT || 3000);
+const port = (process.env.PORT || 8000);
 
 app.listen(port, function() {
   console.log("WikiGuesser is running on http://localhost:" + port);
